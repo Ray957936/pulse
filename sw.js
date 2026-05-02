34456980aad972e959ec928b277fa147b209ff56
Ray <ray@pulse.app>
2026-05-02 21:24:40 +0800

    Fix PWA paths for GitHub Pages subdirectory

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pulse-v1';
-const URLS=['/','/index.html','/manifest.json'];
+const CACHE='pulse-v2';
+const URLS=['/pulse/','/pulse/index.html','/pulse/manifest.json'];
 
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
